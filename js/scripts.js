3cf1c4b7cfbe2dcb34b5261432351b2bd7bff3a5
proxna <pawel.gajzler.pg@gmail.com>
2016-08-12 20:07:03 +0200

    new description and bug fixed

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+/**
+ * Created by GPDellKonto on 2016-07-26.
+ */
+$(document).ready(function () {
+    $('#stickoftruth').click(function(){
+        window.open("https://play.google.com/store/apps/details?id=com.pawelg.paweg.stickoftruth&hl=pl", "_blank");
+    });
+    $('#PoEcalc').click(function(){
+        window.open("https://play.google.com/store/apps/details?id=com.pawelg.paweg.stickoftruth&hl=pl", "_blank");
+    });
+    $('#sortMan').click(function(){
+        window.open("https://play.google.com/store/apps/details?id=com.pawelg.paweg.stickoftruth&hl=pl", "_blank");
+    });
+});
