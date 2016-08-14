2dd6f6bed2c9517647f039021b4ab57251e94279
proxna <pawel.gajzler.pg@gmail.com>
2016-08-14 17:15:59 +0200

    cover images animation added

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 /**
  * Created by GPDellKonto on 2016-07-26.
  */
+var imgCouter=1;
+var imageTable=['images/cover1.png', 'images/cover2.png', 'images/cover3.png'];
 $(document).ready(function () {
     $('#stickoftruth').click(function(){
         window.open("https://play.google.com/store/apps/details?id=com.pawelg.paweg.stickoftruth&hl=pl", "_blank");
@@ -11,4 +13,10 @@ $(document).ready(function () {
     $('#sortMan').click(function(){
         window.open("https://play.google.com/store/apps/details?id=com.FatPixel.sorman", "_blank");
     });
+    window.setInterval(function(){
+        $('body').fadeTo('slow', 0.3, function(){
+            $(this).css('background-image', 'url('+ imageTable[imgCouter%3]+')');
+        }).fadeTo('slow',1);
+        ++imgCouter
+    }, 3000);
 });
